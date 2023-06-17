dc4cef53740c8d2d7c1013e82c631cb57b79c9e3
Preocts <preocts@preocts.com>
2023-06-17 17:10:51 -0400

    Create directory to metric dimension sanitization
    As the directory paths (walk's root) will be used a the dimension for
    the metric we want:
    
    - enforced lower case
    - all "\" and "//" to be replaced with "."
    - all spaces to be replaced with "_"

---
+++ tests/walkwatcher_test.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import pytest
+
+from walk_watcher import walkwatcher
+
+
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("", ""),
+        ("foo", "foo"),
+        ("foo/bar", "foo.bar"),
+        ("foo/bar/baz", "foo.bar.baz"),
+        ("Foo/Bar/Baz", "foo.bar.baz"),
+        ("Foo\\Bar\\Baz", "foo.bar.baz"),
+        ("Foo Bar baz", "foo_bar_baz"),
+    ],
+)
+def test_sanitize_directory_path(path: str, expected: str) -> None:
+    assert walkwatcher._sanitize_directory_path(path) == expected
