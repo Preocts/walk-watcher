14617de00541a7640476420f37a066bb91055326
Preocts <preocts@preocts.com>
2023-06-17 21:49:58 -0400

    Create storeDB and initial tables

--- src/walk_watcher/walkwatcher.py
+++ src/walk_watcher/walkwatcher.py
@@ -1,6 +1,58 @@
 from __future__ import annotations
 
+import logging
 import re
+import sqlite3
+
+
+class StoreDB:
+    """Database for storing data about files and directories."""
+
+    logger = logging.getLogger("walk_watcher.StoreDB")
+
+    def __init__(self, path: str) -> None:
+        """Initialize a new StoreDB connected to the given path."""
+        self.logger.debug("Initializing StoreDB at %s", path)
+        self._connection = sqlite3.connect(path)
+        self._create_file_table()
+        self._create_directory_table()
+
+    def _create_file_table(self) -> None:
+        """Create the file table if it does not already exist."""
+        # We care about the age of files so we need to store the first and last
+        # time we saw them. We also need to store the root directory so we can
+        # tell if a file has been moved.
+        self._connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS files (
+                id INTEGER PRIMARY KEY,
+                root TEXT NOT NULL,
+                filename TEXT NOT NULL,
+                first_seen INTEGER NOT NULL,
+                last_seen INTEGER NOT NULL,
+                removed INTEGER NOT NULL,
+                UNIQUE(root, filename)
+            )
+            """
+        )
+        self.logger.debug("Created file table")
+
+    def _create_directory_table(self) -> None:
+        """Create the directory table if it does not already exist."""
+        # We care about the file count of the directory so that we can map
+        # the activity of the directory over time.
+        self._connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS directories (
+                id INTEGER PRIMARY KEY,
+                root TEXT NOT NULL,
+                last_seen INTEGER NOT NULL,
+                file_count INTEGER NOT NULL,
+                UNIQUE(root)
+            )
+            """
+        )
+        self.logger.debug("Created directory table")
 
 
 def _sanitize_directory_path(path: str) -> str:
--- tests/walkwatcher_test.py
+++ tests/walkwatcher_test.py
@@ -5,6 +5,11 @@ import pytest
 from walk_watcher import walkwatcher
 
 
+@pytest.fixture
+def store_db() -> walkwatcher.StoreDB:
+    return walkwatcher.StoreDB(":memory:")
+
+
 @pytest.mark.parametrize(
     "path, expected",
     [
@@ -19,3 +24,21 @@ from walk_watcher import walkwatcher
 )
 def test_sanitize_directory_path(path: str, expected: str) -> None:
     assert walkwatcher._sanitize_directory_path(path) == expected
+
+
+def test_create_file_table(store_db: walkwatcher.StoreDB) -> None:
+    store_db._create_file_table()
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = [name[0] for name in cursor.fetchall()]
+
+    assert "files" in tables
+
+
+def test_create_directory_table(store_db: walkwatcher.StoreDB) -> None:
+    store_db._create_directory_table()
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = [name[0] for name in cursor.fetchall()]
+
+    assert "directories" in tables
