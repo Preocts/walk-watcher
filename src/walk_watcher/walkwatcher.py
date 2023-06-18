11c4c81a1f6dc7c571a297617ff47043fdfdd9e1
Preocts <preocts@preocts.com>
2023-06-17 22:59:44 -0400

    Handle saving directory and file rows in database

--- src/walk_watcher/walkwatcher.py
+++ src/walk_watcher/walkwatcher.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
 
+import dataclasses
 import logging
 import re
 import sqlite3
+from contextlib import closing
+
+
+@dataclasses.dataclass(frozen=True)
+class Directory:
+    """A directory."""
+
+    root: str
+    last_seen: int
+    file_count: int
+
+
+@dataclasses.dataclass(frozen=True)
+class File:
+    """A file."""
+
+    root: str
+    filename: str
+    last_seen: int
 
 
 class StoreDB:
@@ -47,12 +67,71 @@ class StoreDB:
                 id INTEGER PRIMARY KEY,
                 root TEXT NOT NULL,
                 last_seen INTEGER NOT NULL,
-                file_count INTEGER NOT NULL,
-                UNIQUE(root)
+                file_count INTEGER NOT NULL
             )
             """
         )
         self.logger.debug("Created directory table")
+
+    def save_directories(self, directories: list[Directory]) -> None:
+        """Save the given directories to the database."""
+        self.logger.debug("Saving %s directories", len(directories))
+        with closing(self._connection.cursor()) as cursor:
+            cursor.executemany(
+                """
+                INSERT INTO directories (root, last_seen, file_count)
+                VALUES (?, ?, ?)
+                """,
+                [
+                    (directory.root, directory.last_seen, directory.file_count)
+                    for directory in directories
+                ],
+            )
+            self._connection.commit()
+
+    def save_files(self, files: list[File]) -> None:
+        """Save the given files to the database."""
+        # We run a batch update followed by a batch insert which ignores
+        # errors. This is because we want to update the last_seen time and
+        # removed of existing files and insert new files.
+        self.logger.debug("Saving %s files", len(files))
+        with closing(self._connection.cursor()) as cursor:
+            self._mark_all_removed(cursor)
+            self._update_files(cursor, files)
+            self._insert_files(cursor, files)
+
+            self._connection.commit()
+
+    def _mark_all_removed(self, cursor: sqlite3.Cursor) -> None:
+        """Mark all files as removed, to be updated later."""
+        self.logger.debug("Marking all files as removed")
+        cursor.execute("UPDATE files SET removed = 1 WHERE removed = 0")
+
+    def _insert_files(self, cursor: sqlite3.Cursor, files: list[File]) -> None:
+        """Insert the given files into the database."""
+        self.logger.debug("Inserting %s files", len(files))
+        cursor.executemany(
+            """
+            INSERT OR IGNORE INTO files (root, filename, first_seen, last_seen, removed)
+            VALUES (?, ?, ?, ?, 0)
+            """,
+            [
+                (file.root, file.filename, file.last_seen, file.last_seen)
+                for file in files
+            ],
+        )
+
+    def _update_files(self, cursor: sqlite3.Cursor, files: list[File]) -> None:
+        """Update the given files in the database."""
+        self.logger.debug("Updating %s files", len(files))
+        cursor.executemany(
+            """
+            UPDATE files
+            SET last_seen = ?, removed = 0
+            WHERE root = ? AND filename = ?
+            """,
+            [(file.last_seen, file.root, file.filename) for file in files],
+        )
 
 
 def _sanitize_directory_path(path: str) -> str:
--- tests/walkwatcher_test.py
+++ tests/walkwatcher_test.py
@@ -1,13 +1,40 @@
 from __future__ import annotations
 
+import json
+from pathlib import Path
+from random import shuffle
+
 import pytest
 
 from walk_watcher import walkwatcher
+from walk_watcher.walkwatcher import Directory
+from walk_watcher.walkwatcher import File
+
+DIRECTORIES_FILE = Path(__file__).parent / "directories.json"
+FILES_FILE = Path(__file__).parent / "files.json"
 
 
 @pytest.fixture
 def store_db() -> walkwatcher.StoreDB:
     return walkwatcher.StoreDB(":memory:")
+
+
+@pytest.fixture
+def directories() -> list[Directory]:
+    """Provide a shuffled list of directories."""
+    with DIRECTORIES_FILE.open() as f:
+        directories = [Directory(**d) for d in json.load(f)]
+    shuffle(directories)
+    return directories
+
+
+@pytest.fixture
+def files() -> list[File]:
+    """Provide a shuffled list of files."""
+    with FILES_FILE.open() as f:
+        files = [File(**d) for d in json.load(f)]
+    shuffle(files)
+    return files
 
 
 @pytest.mark.parametrize(
@@ -42,3 +69,112 @@ def test_create_directory_table(store_db: walkwatcher.StoreDB) -> None:
     tables = [name[0] for name in cursor.fetchall()]
 
     assert "directories" in tables
+
+
+def test_save_directories(
+    store_db: walkwatcher.StoreDB,
+    directories: list[Directory],
+) -> None:
+    store_db.save_directories(directories)
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT * FROM directories")
+    rows = cursor.fetchall()
+
+    assert len(rows) == len(directories)
+    assert all(
+        row[1:] == (directory.root, directory.last_seen, directory.file_count)
+        for row, directory in zip(rows, directories)
+    )
+
+
+def test_save_files_empty_rows(
+    store_db: walkwatcher.StoreDB,
+    files: list[File],
+) -> None:
+    store_db.save_files(files)
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT * FROM files")
+    rows = cursor.fetchall()
+
+    assert len(rows) == len(files)
+    assert all(
+        row[1:]
+        == (
+            file.root,
+            file.filename,
+            file.last_seen,
+            file.last_seen,
+            0,
+        )
+        for row, file in zip(rows, files)
+    )
+
+
+def test_save_file_existing_row_updated(
+    store_db: walkwatcher.StoreDB,
+    files: list[File],
+) -> None:
+    store_db.save_files(files)
+
+    # Replace last file with a new one with the same root and filename
+    expected_first_seen = files[-1].last_seen
+    files[-1] = File(
+        root=files[-1].root,
+        filename=files[-1].filename,
+        last_seen=1234567890,
+    )
+
+    store_db.save_files(files)
+
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT * FROM files")
+    last_row = cursor.fetchall()[-1]
+
+    assert last_row[1:] == (
+        files[-1].root,
+        files[-1].filename,
+        expected_first_seen,
+        files[-1].last_seen,
+        0,
+    )
+
+
+def test_save_file_add_new_row_with_existing_rows(
+    store_db: walkwatcher.StoreDB,
+    files: list[File],
+) -> None:
+    store_db.save_files(files)
+
+    # Add a new file with a different root and filename
+    new_file = File(root="new_root", filename="new_file", last_seen=1234567890)
+    files.append(new_file)
+
+    store_db.save_files(files)
+
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT * FROM files")
+    last_row = cursor.fetchall()[-1]
+
+    assert last_row[1:] == (
+        new_file.root,
+        new_file.filename,
+        new_file.last_seen,
+        new_file.last_seen,
+        0,
+    )
+
+
+def test_save_files_marks_all_removed(
+    store_db: walkwatcher.StoreDB,
+    files: list[File],
+) -> None:
+    store_db.save_files(files)
+
+    # Save an empty list of files
+    store_db.save_files([])
+
+    cursor = store_db._connection.cursor()
+    cursor.execute("SELECT removed FROM files")
+    removed = [value[0] for value in cursor.fetchall()]
+
+    assert all(removed)
