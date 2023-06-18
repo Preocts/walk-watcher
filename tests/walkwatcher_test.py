2c074be613b4c73ba5dae3c214176bedf5d3aca1
Preocts <preocts@preocts.com>
2023-06-18 03:48:56 -0400

    Account for timestamps returning native times

--- tests/walkwatcher_test.py
+++ tests/walkwatcher_test.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 import pytest
 
 from walk_watcher.walkwatcher import Directory
@@ -8,7 +10,10 @@ from walk_watcher.walkwatcher import File
 
 def test_model_directory_str() -> None:
     directory = Directory("/foo/bar", 1234567890, 42)
-    assert str(directory) == "/foo/bar (42 files, last seen 2009-02-13 18:31:30)"
+    expected_ts = datetime.fromtimestamp(1234567890).strftime("%Y-%m-%d %H:%M:%S")
+    expected = f"/foo/bar (42 files, last seen {expected_ts})"
+
+    assert str(directory) == expected
 
 
 def test_model_directory_as_metric_line() -> None:
@@ -45,15 +50,13 @@ def test_sanitize_directory_path(path: str, expected: str) -> None:
 def test_model_file_str() -> None:
     file_present = File("/foo/bar", "baz.txt", 1234568190, 1234567890, 300, 0)
     file_removed = File("/foo/bar", "baz.txt", 1234568190, 1234567890, 300, 1)
+    expected_ts = datetime.fromtimestamp(1234568190).strftime("%Y-%m-%d %H:%M:%S")
+    expected = f"/foo/bar/baz.txt (300 seconds old, last seen {expected_ts})"
+    expected_preset = "(present)"
+    expected_removed = "(removed)"
 
-    assert (
-        str(file_present)
-        == "/foo/bar/baz.txt (300 seconds old, last seen 2009-02-13 18:36:30) (present)"
-    )
-    assert (
-        str(file_removed)
-        == "/foo/bar/baz.txt (300 seconds old, last seen 2009-02-13 18:36:30) (removed)"
-    )
+    assert str(file_present) == f"{expected} {expected_preset}"
+    assert str(file_removed) == f"{expected} {expected_removed}"
 
 
 def test_model_file_as_metric_line() -> None:
