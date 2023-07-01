6a1067bcace821be6954c279b500332950343f58
Preocts <preocts@preocts.com>
2023-06-30 23:03:28 -0400

    Create smoketest config and a smoketest runner

---
+++ smoketest/smoketest_watcher.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import logging
+import time
+
+from smoketest_queues import smoketest_runner
+
+from walk_watcher.watcher import Watcher
+from walk_watcher.watcherconfig import WatcherConfig
+
+
+CONFIG = "smoketest.ini"
+WATCHER_SECONDS_INTERVAL = 5
+
+
+def main() -> int:
+    """Run the smoketest watcher."""
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+    config = WatcherConfig(CONFIG)
+    watcher = Watcher(config)
+    next_run = time.time() + WATCHER_SECONDS_INTERVAL
+
+    with smoketest_runner():
+        while "the world keeps turning":
+            try:
+                if time.time() >= next_run:
+                    print("Running smoketest watcher")
+                    watcher.run()
+                    next_run = time.time() + WATCHER_SECONDS_INTERVAL
+                time.sleep(0.1)
+
+            except KeyboardInterrupt:
+                break
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
