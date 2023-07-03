f62e25b616e2695187b2552affc23d39f8197a9f
Preocts <preocts@preocts.com>
2023-07-02 21:59:03 -0400

    Use new entry points

--- smoketest/smoketest_watcher.py
+++ smoketest/smoketest_watcher.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import logging
-import time
 
 from smoketest_queues import smoketest_runner
 
@@ -10,8 +9,6 @@ from walk_watcher.watcherconfig import WatcherConfig
 
 
 CONFIG = "smoketest.ini"
-WATCHER_SECONDS_INTERVAL = 5
-EMIT_SECONDS_INTERVAL = 60
 
 
 def main() -> int:
@@ -19,26 +16,9 @@ def main() -> int:
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
     config = WatcherConfig(CONFIG)
     watcher = Watcher(config)
-    next_run = time.time() + WATCHER_SECONDS_INTERVAL
-    next_emit = time.time() + EMIT_SECONDS_INTERVAL
 
     with smoketest_runner():
-        while "the world keeps turning":
-            try:
-                if time.time() >= next_run:
-                    print("Running smoketest watcher")
-                    watcher.run()
-                    next_run = time.time() + WATCHER_SECONDS_INTERVAL
-
-                if time.time() >= next_emit:
-                    print("Emitting smoketest watcher metrics")
-                    watcher.emit()
-                    next_emit = time.time() + EMIT_SECONDS_INTERVAL
-
-                time.sleep(0.1)
-
-            except KeyboardInterrupt:
-                break
+        watcher.run_loop()
 
     return 0
 
